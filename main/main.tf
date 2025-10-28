80a272db8e980cc35b4216c3a5f8355811da695f
Ricky25119 <aarizmohammad92@gmail.com>
2025-10-28 12:54:41 +0000

    Add files via upload

---
+++ main/main.tf
@@ -0,0 +1,36 @@
+module "rg" {
+  source   = "../main/modules/rg"
+  name     = "sample"
+  location = "west Europe" 
+}
+
+locals {
+  users = jsondecode(var.pim_users)
+
+  pim_assignments = {
+    for user in local.users : user.name => {
+      type            = user.type
+      mail_nickname   = user.mail_nickname
+      justification   = "Automated PIM assignment"
+      assignment_type = lookup(user, "assignment_type", "Eligible")
+      schedule = {
+        expiration = {
+          duration_days  = lookup(user, "duration_days", null)
+          duration_hours = lookup(user, "duration_hours", null)
+        }
+      }
+      roles = {
+        for role in user.roles : role.name => {
+          scopes = [
+            module.rg.rg_id
+          ]
+        }
+      }
+    }
+  }
+}
+
+module "pim" {
+  source = "./modules/pim"
+  pim_assignments = local.pim_assignments
+}
---
+++ main/variable.tf
@@ -0,0 +1,8 @@
+variable "location" {
+  type = string
+}
+
+variable "pim_users" {
+  description = "List of PIM user configurations"
+  type        = string
+}
