15c9dbce8b4c017872ca44a25add552971319441
Ricky25119 <aarizmohammad92@gmail.com>
2025-11-02 01:47:38 +0000

    Add pim_groups variable to variable.tf

--- main/variable.tf
+++ main/variable.tf
@@ -6,3 +6,9 @@ variable "pim_users" {
   description = "List of PIM user configurations"
   type        = string
 }
+
+variable "pim_groups" {
+  description = "List of PIM group configurations"
+  type        = string
+}
+
