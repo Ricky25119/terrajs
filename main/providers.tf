76876ccd51ebaf370185f3b0753e6a0b1777cd6a
Ricky25119 <aarizmohammad92@gmail.com>
2025-11-02 01:48:49 +0000

    Add azuread provider to Terraform configuration

--- main/providers.tf
+++ main/providers.tf
@@ -4,10 +4,19 @@ terraform {
       source = "hashicorp/azurerm"
       version = "4.50.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "azurerm" {
   # Configuration options
   features {}
+  
+}
+
+provider "azuread" {
+  # Configuration is typically inherited from the environment (Azure CLI, SP credentials, etc.)
 }
